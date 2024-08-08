0290dfc4fa7337221adf9dbaf4d17df0f5856144
Erwan Barbedor <erwan@barbedor.bzh>
2024-08-08 08:42:08 +0200

    Rewrite for macro

--- macros/controls.lua
+++ macros/controls.lua
@@ -14,123 +14,95 @@ You should have received a copy of the GNU General Public License along with Plu
 
 -- Define for, while, if, elseif, else control structures
 
-txe.register_macro("for", {"iterator", "body"}, {}, function(args)
-    -- Have the same behavior of the lua for control structure.
-    -- Error management implementation isn't done yet
+--- for control structure
+-- This 'for' macro implements a custom iteration mechanism
+-- that mimics Lua's for loop behavior.
+-- It supports both numeric and generic for loop syntaxes:
+--    - Numeric: for i = start, end, step do ... end
+--    - Generic: for k, v in pairs(t) do ... end
+-- @param iterator A string representing the loop initialization
+-- @param body The content to be executed for each iteration
+-- @usage
+--    \for{i = 1, 10}{ Iteration #i }
+--    \for{k, v in pairs(table)}{ #k : #v }
 
+txe.register_macro("for", {"iterator", "body"}, {}, function(args)
+    -- The macro uses coroutines to handle the iteration process, which allows for flexible
+    -- iteration over various types of iterables without implementing a full Lua parser.
+    
     local result = {}
     local iterator_source = args.iterator:source ()
     local var, var1, var2, first, last
 
-    -- I'm not going to write a full lua parser to read the iterator.
-    -- Some are relatively simple to handle using load, such as
-    -- "for k, v in pairs(t)". But when writing "for=from, to, step"
-    -- each element is an expression in its own. So it's difficult
-    -- to parse, and lua doesn't provide a simple
-    -- mechanism for emulating this syntax.
-    -- So, in very simple cases, the iterator will be parsed for performance, (WIP)
-    -- otherwise we'll switch to using coroutines.
-
     local mode = 1
 
-    -- Check i=1, 10 syntax
-    var, first, last = iterator_source:match('%s*(.-)%s*=%s*([0-9]-)%s*,%s*([0-9]-)$')
-
-    -- If fail, capture anything after "="
+    -- Try to parse the iterator syntax
+    -- First, attempt to match the "var = iterator" syntax
     if not var then
-        mode = mode + 1
-    
         var, iterator = iterator_source:match('%s*([a-zA-Z_][a-zA-Z0-9_]*)%s*=%s*(.-)$')
     end
 
-    -- If fail again, capture anythin after 'in'
+    --- If the first attempt fails, try to match the "var in iterator" syntax
     if not var then
-        mode = mode + 1
         var, iterator = iterator_source:match('%s*(.-)%s*in%s*(.-)$')
     end
     
+    -- If both attempts fail, raise an error
     if not var then
         txe.error(args.iterator, "Non valid syntax for iterator.")
     end
 
-    if mode == 1 then
-        for i=first, last do
-            -- For some reasons, i is treated as a float...
-            i = math.floor(i)
-            
-            -- Add counter to the local scope, to 
-            -- be used by user
-            txe.scope_set_local(var, i)
-            
-            table.insert(result, args.body:render())
-        end
-    elseif mode == 2 then
-        local coroutine_code = "for " .. iterator_source .. " do"
-        coroutine_code = coroutine_code .. " coroutine.yield(" .. var .. ")"
-        coroutine_code = coroutine_code .. " end"
-
-        local iterator_coroutine = txe.load_lua_chunk (coroutine_code, _, _, txe.current_scope ())
-        local co = coroutine.create(iterator_coroutine)
-        while true do
-            local sucess, value = coroutine.resume(co)
-            if not value then
-                break
-            end
-            if not sucess or not co then
-                txe.error(args.iterator, "(iterator error)" .. value)
-            end
-
-            txe.scope_set_local (var, value)
-            table.insert(result, args.body:render())
-        end
-    
-    elseif mode == 3 then
-        -- Save all variables name in a table
-        local variables_list = {}
-        for name in var:gmatch('[^%s,]+') do
-            table.insert(variables_list, name)
-        end
-        
-        -- Create the iterator
-        local iter, state, key = txe.eval_lua_expression (args.iterator, iterator)
-        
-        -- Check if state is non nil
-        if state == nil then
-            txe.error(args.iterator, "fail to make the iterator.")
-        end
-
-        -- Check if iter is callable.
-        if type(iter) ~= "function" or type(iter) == "table" and not getmetatable(iter).__call then
-            txe.error(args.iterator, "iterator cannot be '" .. type(iter) .. "'")
-        end
-
-        -- Get first iteration
-        local values_list = { iter(state, key) }
-
-        -- If the iterator return nothing
-        if #values_list == 0 then
-            return ""
-        end
-
-        -- If not enough (or too much) variables was provided
-        if #values_list ~= #variables_list then
-            txe.error(args.iterator, "Wrong number of variables, " .. #variables_list .. " instead of " .. #values_list .. "." )
-        end
-
-        -- Run util the iterator return nothing
-        while values_list[1] do
-            -- Add all returned variables to the local scope
-            for i=1, #variables_list do
-                txe.scope_set_local (variables_list[i], values_list[i])
-            end
-
-            table.insert(result, args.body:render())
-
-            -- Call the iterator one more time
-            values_list = { iter(state, values_list[1]),  }
-        end
+    -- Extract all variable names from the iterator
+    local variables_list = {}
+    for name in var:gmatch('[^%s,]+') do
+        table.insert(variables_list, name)
     end
 
+    -- Construct a Lua coroutine to handle the iteration
+    local coroutine_code = "for " .. iterator_source .. " do"
+    coroutine_code = coroutine_code .. " coroutine.yield(" .. var .. ")"
+    coroutine_code = coroutine_code .. " end"
+
+    -- Load and create the coroutine
+    local iterator_coroutine = txe.load_lua_chunk (coroutine_code, _, _, txe.current_scope ())
+    local co = coroutine.create(iterator_coroutine)
+    
+    -- Main iteration loop
+    while true do
+        -- Resume the coroutine to get the next set of values
+        local values_list = { coroutine.resume(co) }
+        local sucess = values_list[1]
+        table.remove(values_list, 1)
+        local first_value = values_list[1]
+            
+        -- Break the loop if there are no more values
+        if not first_value then
+            break
+        end
+
+        -- Check for Lua errors in the coroutine
+        if not sucess or not co then
+            txe.error(args.iterator, "(lua error)" .. first_value:gsub('.-:[0-9]+:', ''))
+        end
+
+        -- Verify that the number of variables matches the number of values
+        if #values_list ~= #variables_list then
+            txe.error(args.iterator,
+                "Wrong number of variables, "
+                .. #variables_list
+                .. " instead of "
+                .. #values_list .. "." )
+        end
+
+        -- Set local variables in the current scope
+        for i=1, #variables_list do
+            txe.scope_set_local (variables_list[i], values_list[i])
+        end
+
+        -- Render the body of the loop and add it to the result
+        table.insert(result, args.body:render())
+    end
+    
     return table.concat(result, "")
 end)
 
